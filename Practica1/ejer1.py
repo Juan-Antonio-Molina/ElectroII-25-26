01c9e65e0b98f6817ef357a80d7e42b2c7eb163a
Juan-Antonio-Molina <juanantonio.molinab@um.es>
2025-09-20 20:24:02 +0200

    cambios menores

--- Practica1/ejer1.py
+++ Practica1/ejer1.py
@@ -3,7 +3,7 @@ ejer1.py - Ejercicio 1: Campo de una carga puntual
 
 Objetivos:
 - Calcule y visualice el potencial y campo elécitrco de una carga puntual
-- Deemuestre numéricamente que el campo cumple la ley de Coulomb, i.e., E = K 1/r^2
+- Demuestre numéricamente que el campo cumple la ley de Coulomb, i.e., E = K 1/r^2
 - Genere una gráfica del campo vs. distancia e interprete la relación (1/r)
 
 Created on Fri Sept 19 22:07:51 2025
@@ -27,32 +27,31 @@ X,Y = np.meshgrid(x, y)
 V = potencial_carga(q, r, X, Y)
 Ex, Ey = campo_carga(q, r, X, Y)
 
-#Visualizar
+#Visualizamos el potencial y el campo
 fig, ax = plt.subplots(figsize=(8, 6))
 ax.scatter(r[0], r[1], c=colors[0])
 
-#pintar potencial
+# Potencial
 niveles=[-50, -10, -5, -1, 0, 1, 5, 10, 50]
 contours = ax.contour(X, Y, V, levels=niveles, colors='black',
                      alpha=0.7)
 ax.clabel(contours, inline=True, fontsize=10, fmt='%.1f')
 ax.clabel(contours, inline=True, fontsize=8)
 
-#pintar campo
+# Campo
 magnitude = np.sqrt(Ex**2 + Ey**2)
 streamplot = ax.streamplot(X, Y, Ex, Ey, color=magnitude,
                           cmap='viridis', density=1.,
                           linewidth=1, arrowsize=1.2)
-#Aspecto
+
 ax.set_xlabel('X [m]')
 ax.set_ylabel('Y [m]')
 ax.set_title('Potencial y Campo Eléctrico de una carga puntual')
 ax.set_aspect('equal')
-
-# Nos muestra la imágen
 plt.show()
 
 # -----------------------------------------------------------------------
+#                          NUEVO OBJETIVO
 # -----------------------------------------------------------------------
 # Tomamos puntos de la horizontal (simetría radial)
 x2 = np.linspace(0.5, 2.5, 50)
@@ -67,35 +66,28 @@ print(f"Coef. correlación Pearson r = {res.rvalue:.3f}")
 print(f"R^2 (calidad ajuste) = {res.rvalue**2:.3f}")
 
 # Dibujamos el ajuste
-
-# Calcular la línea de ajuste
 y_ajuste = res.slope * inv_dist_cuadrado + res.intercept
 
-# Dibujar los puntos y el ajuste en un nuevo subplot
 fig_ajuste, ax_ajuste = plt.subplots(figsize=(8, 6))
 ax_ajuste.scatter(inv_dist_cuadrado, E2x, label='Puntos de datos')
 ax_ajuste.plot(inv_dist_cuadrado, y_ajuste, color='red', label='Ajuste Lineal')
 
-# Etiquetas y leyenda
-ax_ajuste.set_xlabel('1/distancia^2')
-ax_ajuste.set_ylabel('Componente E')
-ax_ajuste.set_title('Ajuste Lineal del Campo Eléctrico')
+ax_ajuste.set_xlabel('1/r^2 [m^-2]')
+ax_ajuste.set_ylabel('E [N m^-1]')
+ax_ajuste.set_title('Ajuste Lineal del campo eléctrico frente a 1/r^2')
 ax_ajuste.legend()
-
 plt.show()
 
 # -----------------------------------------------------------------------
+#                          NUEVO OBJETIVO
 # -----------------------------------------------------------------------
-inv_dist = x2 ** (-1)
 # Dibujar los puntos y el ajuste en un nuevo subplot
 fig_ajuste, ax_ajuste = plt.subplots(figsize=(8, 6))
-ax_ajuste.scatter(inv_dist, E2x, label='Puntos de datos')
-ax_ajuste.plot(inv_dist, E2x, color='red', label='funcion')
+ax_ajuste.scatter(x2, E2x, label='Puntos de datos')
+ax_ajuste.plot(x2, E2x, color='red', label='funcion')
 
-# Etiquetas y leyenda
-ax_ajuste.set_xlabel('1/distancia')
-ax_ajuste.set_ylabel('Componente E')
-ax_ajuste.set_title('Ajuste Lineal del Campo Eléctrico')
+ax_ajuste.set_xlabel('r [m]')
+ax_ajuste.set_ylabel('E [N m^-1]')
+ax_ajuste.set_title('Comparación del campo eléctrico frente a la distancia')
 ax_ajuste.legend()
-
 plt.show()
