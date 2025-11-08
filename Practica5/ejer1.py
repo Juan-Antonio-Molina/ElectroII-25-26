4d687c9cb880dd6d88ada4842a06211218438fe2
ManuelPividal <manuelpividal@outlook.es>
2025-11-08 18:39:13 +0100

    There are some problems with the calculation of the wavelength, my folk. I left there some comments, but I hope we will solve it soon! Then we will discuss what to do with the Poynting vector. Nevertheless, GOOD JOB!!

--- Practica5/ejer1.py
+++ Practica5/ejer1.py
@@ -12,25 +12,14 @@ L = 20
 Nz = 300
 z = np.linspace(0, L, Nz + 1)
 dz = z[2] - z[1]
+loon = L / 3  # pintamos 3 longitudes de onda
+k = 2 * pi / loon
 
 # Discretización temporal
 dt = 0.99 * dz / v
 Nt = 2 * Nz
-T = 10
-loon = v * T
-k = 2 * pi / loon
 w = v * k
 T = 2 * pi / w
-
-# Caracteristicas del medio
-print(f" Características para medio con e_r = {epr :.2f} y u_r = {mur :.2f}")
-print(" ")
-print(f"Periodo = {T:.4f}")
-print(f"Longitud de onda = {loon:.4f}")
-print(f"Constante de propagacion = {k:.4f} ")
-print(f"Frecuencia = {w:.4f}")
-print(f"Velocidad de onda = {v:.4f}")
-
 
 # Inicialización
 F = np.zeros([Nz + 1, Nt + 1])  # F es Psi (función de onda)
@@ -42,64 +31,40 @@ F[0, 1] = F0 * np.sin(-w * 1 * dt)  # Inicializacion
 A = (v * dt / dz) ** 2
 r = v * dt / dz
 
+# Solucion real
+def solucion_analitica(z, t):
+    """
+    Psi_analitica(z, t) = Psi_0 * sin(omega * z / v - omega * t)
+    """
+    return F0 * np.cos(k*z - w*t - pi/2)
+
 # Cálculo
 for j in np.arange(1, Nt):
     F[0, j+1] = F0*np.sin(-w*j*dt)
-    F_teo[0, j + 1] = F0 * np.sin(-w * j * dt)
     for i in np.arange(1, Nz):
         F[i,j + 1] = A * (F[i + 1, j] + F[i - 1, j]) + 2 * (1 - A) * F[i, j] - F[i, j - 1]
-        F_teo[i, j+1] = F0 * np.cos(k * i*dz - w * j*dt - pi / 2)
+        F_teo[i, j+1] = solucion_analitica(i*dz, j*dt)
 
     # Condición de pared absorbente
     F[Nz, j+1] = F[Nz-1, j] + (r-1)/(r+1) * ( F[Nz-1, j+1] - F[Nz, j] )
 
 
-# Bloque de animacion
 from matplotlib.animation import FuncAnimation
 
-fig, ax = plt.subplots(figsize=(10, 5))
-line, = ax.plot(z, F[:, 0], lw=2, label='Numérica')  # Inicializa la línea con el tiempo j=0
-line2, = ax.plot(z, F_teo[:, 0], 'r:', lw=2, label='Analítica')
+fig, ax = plt.subplots(figsize=(6, 3))
+line, = ax.plot(z, F[:, 0], lw=2)  # Inicializa la línea con el tiempo j=0
+line2, = ax.plot(z, F_teo[:, 0], 'r:', lw=2)
 ax.set_xlim(0, L), ax.set_ylim(-1.5, 1.5), ax.set_xlabel('z'), ax.set_ylabel('F')
-ax.set_xlabel('Posición z [unidades]'), ax.set_ylabel('Amplitud F(z, t)')
-ax.set_title('Propagación de Onda en el vacio')
-ax.grid(True, linestyle='--', alpha=0.6)
-ax.legend(loc='lower left')
-
 
 for j in range(Nt + 1):
     # Actualiza los datos de la línea con la columna de tiempo 'j' de la matriz F
     line.set_ydata(F[:, j])
     line2.set_ydata(F_teo[:, j])
+
+    # Actualiza el título con el tiempo transcurrido (j*dt)
     ax.set_title(f"t={j * dt:.4f} s")
 
     # Pausa para visualizar la animación
     plt.pause(0.03)  # ~30 ms por frame
 
-plt.show()
-
-# Dibujamos tres periodo
-fig, ax = plt.subplots(figsize=(10, 5))
-j_T = int(3*T/dt)
-ax.plot(z, F[:, j_T], color='darkslategray',label='Numérica')
-ax.plot(z, F_teo[:, j_T], 'r:', label = "Analítica")
-ax.set_xlabel(r'$z$ [m]')
-ax.set_ylabel(r'$F(z,3T)$ [V/m]')
-ax.set_title(f'Evolución espacial del campo eléctrico t={3*T:.2f} s')
-ax.grid(True, linestyle='--')
-ax.legend()
-plt.show()
-
-
-# Comprobacion excitacion inicial
-fig, ax = plt.subplots(figsize=(10, 5))
-j_landa = 0
-t = np.arange(0,(Nt+1)*dt,dt)
-ax.plot(t, F[j_landa, :], color='darkslategray',label='Numérica')
-ax.plot(t, F_teo[j_landa, :], 'r:', label = "Analítica")
-ax.set_xlabel(r'$t$ [s]')
-ax.set_ylabel(r'$F(0,t)$ [V/m]')
-ax.set_title(f'Evolución temporal del campo eléctrico en z={0:.2f} m')
-ax.grid(True, linestyle='--')
-ax.legend()
-plt.show()
+plt.show()  # Muestra
--- Practica5/ejer2.py
+++ Practica5/ejer2.py
@@ -49,6 +49,15 @@ F0 = 1  # Amplitud de la onda de excitación
 # Array A(i) = (v(i) * dt / dz)^2
 A_array = (v_array * dt / dz) ** 2
 
+t_interfaz = []
+t_derecha = []
+tol = 0.001
+amplitud = F0/2
+t_amplitud = []
+t_amplitud_diel = []
+amplitud_diel = F0/3
+tol_amp = 0.01
+
 # --- Bucle de Cálculo ---
 # Inicialización de los dos primeros pasos de tiempo (requerido por el esquema)
 for j in np.arange(0, 2):
@@ -58,7 +67,7 @@ for j in np.arange(0, 2):
 # Propagación principal (pasos de tiempo j=1 hasta Nt-1)
 for j in np.arange(1, Nt):
     # Condición de excitación (fuente) en z=0
-    F[0, j + 1] = F0 * np.sin(w_inc * j * dt)
+    F[0, j + 1] = F0 * np.sin(-w_inc * j * dt)
 
     v_abc = v_array[1]
     r_abc = v_abc * dt / dz
@@ -68,12 +77,12 @@ for j in np.arange(1, Nt):
         A_i = A_array[i]  # Usamos el factor A local
         F[i, j + 1] = A_i * (F[i + 1, j] + F[i - 1, j]) + 2 * (1 - A_i) * F[i, j] - F[i, j - 1]
 
-    F[i_int, j + 1] = F[i_int - 1, j] + (r_abc - 1) / (r_abc + 1) * (F[i_int - 1, j + 1] - F[i_int, j])
+    #F[i_int, j + 1] = F[i_int - 1, j] + (r_abc - 1) / (r_abc + 1) * (F[i_int - 1, j + 1] - F[i_int, j])
 
     v_abc = v_array[Nz]
     r_abc = v_abc * dt / dz
 
-    for i in np.arange(i_int+1, Nz):
+    for i in np.arange(i_int, Nz):
         A_i = A_array[i]  # Usamos el factor A local
         F[i, j + 1] = A_i * (F[i + 1, j] + F[i - 1, j]) + 2 * (1 - A_i) * F[i, j] - F[i, j - 1]
 
@@ -87,6 +96,67 @@ for j in np.arange(1, Nt):
 
     # Simple primer orden Mur:
     F[Nz, j + 1] = F[Nz - 1, j] + (r_abc - 1) / (r_abc + 1) * (F[Nz - 1, j + 1] - F[Nz, j])
+    if np.abs(F[i_int, j]) >tol:
+        t_interfaz.append(j)
+    if np.abs(F[Nz, j]) > tol:
+        t_derecha.append(j)
+
+    if np.abs(F[2,j]-amplitud)<tol_amp:
+        t_amplitud.append(dt * j)
+
+    if np.abs(F[i_int+2,j]-amplitud_diel)<tol_amp:
+        t_amplitud_diel.append(dt * j)
+
+
+# Procedemos a calcular la velocidad de propagación
+# en cada medio dividiendo espacio recorrido por tiempo empleado:
+T_interfaz = t_interfaz[0]*dt
+T_derecha = t_derecha[0]*dt
+
+velocidad_vac = (z[i_int] - z[0]) / T_interfaz
+velocidad_diel = (z[-1] - z[i_int]) / (T_derecha-T_interfaz)
+
+print(f"La velocidad de propagación en el vacío es v={velocidad_vac:.2f}")
+print(f"La velocidad de propagación en el dieléctrico es v={velocidad_diel:.2f}")
+
+# Ahora queremos hallar el periodo en cada caso.
+# Para ello, fijaremos una cierta amplitud y una tolerancia.
+# Veremos cuánto tiempo transcurre entre dos veces consecutivas
+# en las que se alcanza dicha amplitud:
+# Tomaremos el primer valor del periodo que obtengamos, pues después
+# se ven involucrados los efectos de reflexión y transmisión.
+periodo_exp = t_amplitud[1]-t_amplitud[0]
+periodo_diel_exp = t_amplitud_diel[1]-t_amplitud_diel[0]
+print(f"El periodo de nuestra onda en el vacío es T={periodo_exp:.2f}")
+print(f"El periodo de nuestra onda en el dieléctrico es T={periodo_diel_exp:.2f}")
+
+# Para hallar la longitud de onda, fijaremos un instante de tiempo
+# en vez de una posición. Para ese instante de tiempo, veremos cuánta
+# distancia hay entre dos puntos espaciales consecutivos donde se alcance
+# la misma amplitud:
+# A la hora de fijar un instante de tiempo, debemos asegurarnos de que
+# la onda ya haya alcanzado el dieléctrico lo suficiente.
+
+onda_vacio = F[0:i_int, t_interfaz[0]]
+onda_diel = F[i_int:Nz, t_derecha[0]]
+pos_vacio = []
+pos_diel = []
+tol_long = 0.05 # Ponemos amplitudes negativas para que las encuentre
+amplitud_vac = onda_vacio[1]
+amplitud_dielec = onda_diel[1]
+for i in range(i_int):
+    if np.abs(onda_vacio[i]-amplitud_vac) < tol_long:
+        pos_vacio.append(z[i])
+for i in np.arange(i_int, Nz):
+    if np.abs(onda_diel[i-i_int]-amplitud_dielec) < tol_long:
+        pos_diel.append(z[i])
+
+long_vac = pos_vacio[2]-pos_vacio[1]
+long_diel = pos_diel[2]-pos_diel[1]
+print(f"La longitud de onda en el vacío es L={long_vac:.2f}")
+print(f"La longitud de onda en el dieléctrico es L={long_diel:.2f}")
+# ESTO SALE MAL
+# Y SI MEJOR USAMOS LAMBDA=v*T Y YA ESTÁ???
 
 # --- Visualización de la Animación ---
 from matplotlib.animation import FuncAnimation
